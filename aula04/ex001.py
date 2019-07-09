c6d0c55c0b15b94dbace93b20ab129b32627d426
Paulo Arruda <paulo.arruda85@gmail.com>
2019-07-09 20:39:09 -0300

    curso de python em andamento

---
+++ aula04/ex001.py
@@ -0,0 +1,4 @@
+nome = input('Qual seu nome? ')
+idade = input('Qual sua idade? ')
+peso = input('Qual seu peso? ')
+print(nome, idade, peso)
---
+++ aula04/ex004.py
@@ -0,0 +1,4 @@
+p_numero = int(input('Primeiro número '))
+s_numero = int(input('SSegundo número '))
+soma = p_numero + s_numero
+print('A soma é',soma)
