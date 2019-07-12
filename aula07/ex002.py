597637c7ef73f23a5399d0b7842680b33c8de120
Paulo Arruda <paulo.arruda85@gmail.com>
2019-07-11 22:32:29 -0300

    aula 07 - em andamento

---
+++ aula07/ex002.py
@@ -0,0 +1,4 @@
+numero = int(input('Digite um número '))
+sucessor = numero + 1;
+antecessor = numero - 1;
+print('O sucesssor do {} é {}, o antecessor do {} é {}'.format(numero, sucessor, numero, antecessor))
---
+++ aula07/ex003.py
@@ -0,0 +1,4 @@
+numero = int(input('Digite um número '))
+dobro = numero * 2
+raizQuadrada = numero**(1/2)
+print('O dobro do {} é {}, a raiz quadrada de {} é {}'.format(numero, dobro, numero, raizQuadrada))
